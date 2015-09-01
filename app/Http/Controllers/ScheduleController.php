c2d02f6d1c6b16683022e74a3cf266ec13bafede
I823925 <I823925@SLEN00570262A.amer.global.corp.sap>
2015-09-01 19:09:21 -0300

    almost there

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -91,10 +91,21 @@ class ScheduleController extends Controller
         ->join('engineers as e', 's.engineer_id', '=', 'e.id')
         ->join('engineer_areas as ea', 'e.id', '=', 'ea.engineer_id')
         ->join('areas as a', 'ea.area_id', '=', 'a.id')
+        ->select('e.id as engineer_id', 'a.id as area_id', 's.date', 'e.name as engineer', 'e.available', 's.start', 's.end', 'a.name as area', 'a.component')
         ->where('component', 'LIKE', 'MM%')
           ->orderBy('date')
           ->get();
-      return $list;
+
+        $newObj = [];
+
+        $current_date = null;
+        foreach ($list as $i => $obj) {
+          $newObj[$obj->date]['date'] = $obj->date;
+          $obj->onTime = $this->isOnTime($obj->date, $obj->start, $obj->end);
+          $newObj[$obj->date]['data'][] = $obj;
+        }
+
+        return $newObj;
     }
 
     public function getSRM() {
@@ -103,8 +114,48 @@ class ScheduleController extends Controller
         ->join('engineer_areas as ea', 'e.id', '=', 'ea.engineer_id')
         ->join('areas as a', 'ea.area_id', '=', 'a.id')
         ->where('component', 'LIKE', 'SRM%')
-          ->orderBy('date')
-          ->get();
-      return $list;
+        ->orderBy('date')
+        ->get();
+
+      $newObj = [];
+      foreach ($list as $obj => $i) {
+        $newObj[$i]->date = $list->date;
+        //$newObj[$i]->data = $list;
+      }
+
+      return $newObj;
     }
+
+    private function isOnTime($date, $time_start, $time_end) {
+
+      $isOnTime = false;
+
+      $date = explode("-", $date);
+      $time_start = explode(":", $time_start);
+      $time_end = explode(":", $time_end);
+
+      $tz_string = "America/Sao_Paulo";
+      $tz_object = new \DateTimeZone($tz_string);
+
+      $date_start = new \DateTime();
+      $date_start->setTimezone($tz_object);
+      $date_start->setDate($date[0], $date[1], $date[2]);
+      $date_start->setTime($time_start[0], $time_start[1], "00");
+
+      $date_end = new \DateTime();
+      $date_end->setTimezone($tz_object);
+      $date_end->setDate($date[0], $date[1], $date[2]);
+      $date_end->setTime($time_end[0], $time_end[1], "00");
+
+      $now = new \DateTime("now");
+      $now->setTimezone($tz_object);
+
+
+      if ($date_start <= $now && $date_end >= $now) {
+        $isOnTime = true;
+      }
+
+      return $isOnTime;
+    }
+
 }
